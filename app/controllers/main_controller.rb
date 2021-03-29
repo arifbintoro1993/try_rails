d23206a25eab45158ba92a5021f05d34c6336a7a
Arif Bintoro <arif.bintoro@tech.indosterling.com>
2021-03-30 01:26:30 +0700

    [NEW FEATURE]update password

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,2 @@
 class MainController < ApplicationController
-    def index
-        # if session[:user_id] present
-        # find user and set @user
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        end
-    end
 end
