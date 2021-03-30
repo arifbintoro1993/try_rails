f229cb1b4488bfab5d8578ef5c26d2f2600ad1e0
Arif Bintoro <arif.bintoro@tech.indosterling.com>
2021-03-30 16:56:24 +0700

    user model unit testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,20 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "test validate email user" do
+    user = User.new(email: "test")
+    assert_not user.valid?
+  end
+
+  test "test user not save" do
+    user = User.new(email: "test@test")
+    assert_not user.save
+  end
+
+  test "test user save" do
+    user = User.new(email: "test@email", password: "password")
+    assert user.save
+  end
+
 end
